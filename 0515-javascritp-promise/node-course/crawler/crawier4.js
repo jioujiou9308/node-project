1e3273022fcfb5be2f5cb79534dfc6e36e9bea37
jioujiou9308 <p1789632p@gmail.com>
2022-05-21 14:44:18 +0800

    dotenv

--- 0515-javascritp-promise/node-course/crawler/crawier4.js
+++ 0515-javascritp-promise/node-course/crawler/crawier4.js
@@ -5,13 +5,15 @@
 // 引用進來
 const mysql = require('mysql2/promise');
 
+require('dotenv').config();
+
 (async () => {
      const connection = await mysql.createConnection({
-          host: 'localhost',
-          port: 3306,
-          user: 'admin',
-          password: '',
-          database: 'stock',
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT,
+          user: process.env.DB_USER,
+          password: process.env.DB_PWD,
+          database: process.env.DB_DB,
      });
 
      let [data, fields] = await connection.execute('SELECT * FROM stocks');
